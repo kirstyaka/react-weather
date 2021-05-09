61ed9d8228abe5b5492ade0f8d59174de6fe1930
kirstyaka <78785730+kirstyaka@users.noreply.github.com>
2021-05-09 15:12:58 +0100

    updated axios import typo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from axios;
+import axios from 'axios';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1> Hello World</h1>
+        <h1> Hello Kirsty's World</h1>
       </header>
     </div>
   );
