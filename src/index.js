3c62d5bd762009d8551c129e71773e3f7c7ab5eb
kirstyaka <78785730+kirstyaka@users.noreply.github.com>
2021-05-09 18:46:21 +0100

    Setup between services
    Connecting VS Code to GitHub & Netlify

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import { StrictMode } from "react";
 import ReactDOM from 'react-dom';
 import Weather from "./Weather";
 import './Weather.css';
 import reportWebVitals from './reportWebVitals';
 
-
+function App(){
+  return (
     <div className="App">
       <Weather />;
     </div>
+  );
+}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-
+  <StrictMode>
+    <App />
+  </StrictMode>,
   rootElement
 );
