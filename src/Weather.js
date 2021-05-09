88185a2d3045c5d4edb282cb0eb18a80ae7adb10
kirstyaka <78785730+kirstyaka@users.noreply.github.com>
2021-05-09 18:40:40 +0100

    Amended structure to deploy
    Connected VS Code to GitHub & Netlify.

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import './Weather.css';
 
 export default function Weather() {
--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import './Weather.css';
 import reportWebVitals from './reportWebVitals';
 
 
-function App() {
-  return (
     <div className="App">
       <Weather />;
     </div>
-  );
-}
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
